e1b24f29e0916d7d67e0764d54586aeef4a3cc32
Alberto Roman <romanvieito@gmail.com>
2026-01-31 19:17:42 -0500

    Update footer contact information and remove social media links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -60,11 +60,11 @@ export default function Footer() {
               </li>
               <li>
                 <a
-                  href="tel:+13055551234"
+                  href="tel:+17868179906"
                   className="flex items-center gap-3 text-white/60 hover:text-coral transition-colors"
                 >
                   <Phone className="w-5 h-5" />
-                  (305) 555-1234
+                  (786) 817-9906
                 </a>
               </li>
               <li>
@@ -74,29 +74,6 @@ export default function Footer() {
                 </span>
               </li>
             </ul>
-          </div>
-
-          {/* Social */}
-          <div>
-            <h4 className="font-semibold text-white mb-6">Follow Us</h4>
-            <div className="flex gap-4">
-              <motion.a
-                href="#"
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-                className="w-10 h-10 rounded-full bg-white/10 flex items-center justify-center text-white/60 hover:bg-coral hover:text-white transition-all"
-              >
-                <Instagram className="w-5 h-5" />
-              </motion.a>
-              <motion.a
-                href="#"
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-                className="w-10 h-10 rounded-full bg-white/10 flex items-center justify-center text-white/60 hover:bg-coral hover:text-white transition-all"
-              >
-                <Linkedin className="w-5 h-5" />
-              </motion.a>
-            </div>
           </div>
         </div>
 
